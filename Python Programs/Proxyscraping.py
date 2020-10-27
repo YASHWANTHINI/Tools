4a9c878c3b6b29878df6bf67410102acf360f8d6
RakeshElamaran <36146368+rakeshelamaran98@users.noreply.github.com>
2020-10-27 21:58:32 +0530

    Create Proxyscraping.py

---
+++ Python Programs/Proxyscraping.py
@@ -0,0 +1,17 @@
+import requests
+from bs4 import BeautifulSoup
+
+proxyDomain = "https://free-proxy-list.net/"
+
+r = requests.get(proxyDomain)
+
+soup = BeautifulSoup(r.content,'html.parser')
+
+table = soup.find('table',{"id" : "proxylisttable"})
+
+for row in table.find_all('tr'):
+    columns = row.find_all('td')
+    try:
+        print "%s:%s\t%-20s\t%-10s" % (columns[0].get_text(),columns[1].get_text(),columns[3].get_text(),columns[4].get_text())
+    except:
+        pass
